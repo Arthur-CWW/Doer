1647b56ee4e6490273a12b7bd60f0e49306b306c
chun1 <arthur@chunai.dev>
2024-01-28 18:41:01 +1100

    fix database url

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -2,6 +2,8 @@ import type { Config } from "drizzle-kit";
 import * as dotenv from "dotenv";
 
 dotenv.config({ path: "../../.env" });
+// console.log("hheeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+// console.log(process.env);
 
 const uri = [
   "mysql://",
@@ -10,10 +12,13 @@ const uri = [
   process.env.DB_PASSWORD,
   "@",
   process.env.DB_HOST,
-  ":3306/",
+  ":",
+  process.env.DB_PORT,
+  "/",
   process.env.DB_NAME,
-  '?ssl={"rejectUnauthorized":true}',
+  // '?ssl={"rejectUnauthorized":true}',
 ].join("");
+// console.log(uri);
 
 export default {
   schema: "./src/schema",
