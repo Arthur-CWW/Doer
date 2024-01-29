2f9f35e00d876150ba9b6b88c01ed1927ee9a604
chun1 <arthur@chunai.dev>
2024-01-29 23:56:19 +1100

    debugging auth

--- apps/nextjs/src/app/_components/auth-showcase.tsx
+++ apps/nextjs/src/app/_components/auth-showcase.tsx
@@ -3,8 +3,12 @@ import { Button } from "@acme/ui/button";
 
 export async function AuthShowcase() {
   const session = await auth();
-
-  if (!session) {
+  console.log("-----------------------------------------------------------");
+  console.log("session");
+  console.log(session);
+  console.log(typeof session);
+  console.log("-----------------------------------------------------------");
+  if (!session || !session.user) {
     return (
       <form>
         <Button
@@ -19,7 +23,6 @@ export async function AuthShowcase() {
       </form>
     );
   }
-
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <p className="text-center text-2xl">
