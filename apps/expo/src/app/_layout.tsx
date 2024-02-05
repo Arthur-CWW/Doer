d19be6c0c4f6bf19424af59754e210366ce47e3a
chun1 <arthur@chunai.dev>
2024-02-05 23:50:52 +1100

    colour scheme for dark mode

--- apps/expo/src/app/_layout.tsx
+++ apps/expo/src/app/_layout.tsx
@@ -5,19 +5,25 @@ import { TRPCProvider } from "~/utils/api";
 
 import "../styles.css";
 
+import { useColorScheme } from "nativewind";
+
 // This is the main layout of the app
 // It wraps your pages with the providers they need
 export default function RootLayout() {
+  const { colorScheme } = useColorScheme();
   return (
     <TRPCProvider>
       {/*
           The Stack component displays the current page.
-          It also allows you to configure your screens 
+          It also allows you to configure your screens
         */}
       <Stack
         screenOptions={{
           headerStyle: {
             backgroundColor: "#f472b6",
+          },
+          contentStyle: {
+            backgroundColor: colorScheme == "dark" ? "#09090B" : "#FFFFFF",
           },
         }}
       />
